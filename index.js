3850a9272bc9c62d6350879df466c346c79e2562
SMoragon <smoragonj@gmail.com>
2023-11-03 19:31:51 +0100

    EJS structure started
    Dynamic webpages for destinies and header,footer transformewd into ejs to save us time.
    
    Co-Authored-By: ZhiPeng136 <98947090+ZhiPeng136@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,8 +5,63 @@ const path=require("path")
 const port=3000;
 const app=express();
 
+var destinies_json={}  // name, description, photo, price
+
+app.set("view engine", "ejs")
+app.set("views",path.join(__dirname,"views"))
+
 app.use(express.static(path.join(__dirname,"public")))
 
+app.get("/", (request, response)=>{
+    response.status(200).render("index.ejs")
+})
+
+app.get("/index.html", (request, response)=>{
+    response.status(200).render("index.ejs")
+})
+
+app.get("/destinos.html", (request, response)=>{
+    response.status(200).render("destinos.ejs")
+})
+
+app.get("/nosotros.html", (request, response)=>{
+    response.status(200).render("nosotros.ejs")
+})
+
+app.get("/sostenibilidad.html", (request, response)=>{
+    response.status(200).render("sostenibilidad.ejs")
+})
+
+app.get("/reservas.html", (request, response)=>{
+    response.status(200).render("reservas.ejs")
+})
+
+app.get("/destinos/:id", (request, response)=>{
+    var id=request.params.id
+    var context
+
+    if((i in destinies_json)){
+        context=destinies_json[i]
+    }
+    else{
+        context=0//BD
+    }
+
+    response.status(200).render("generalDestiny.ejs",context)
+})
+
+app.get("/destinos/albacete.html", (request, response)=>{
+    //json=consulta  a la BD
+    response.status(200).render("plantillaGeneral.ejs",{datos:(json)})
+})
+
+app.get("/destinos/china.html", (request, response)=>{
+    response.status(200).render("reservas.ejs")
+})
+
+app.use((request,response)=>{
+    response.status(404).send("<h1> Error 404: Route is not defined</h1>")
+})
 
 app.listen(port,(err)=>{
     if(err){
